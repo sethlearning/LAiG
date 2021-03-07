f37dac93a59c195c2e07da4621db817026b86ad3
sethvs <sethvs@users.noreply.github.com>
2021-03-07 16:26:28 +0300

    Scaffolding

--- determinant/run.ps1
+++ determinant/run.ps1
@@ -6,20 +6,119 @@ param($Request, $TriggerMetadata)
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell HTTP trigger function processed a request."
 
+<#
 # Interact with query parameters or the body of the request.
+
 $name = $Request.Query.Name
 if (-not $name) {
     $name = $Request.Body.Name
 }
+#>
 
-$body = "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-
-if ($name) {
-    $body = "Hello, $name. This HTTP triggered function executed successfully."
+if (-not ($size = $Request.Query.Size))
+{
+    $size = 10
 }
 
-# Associate values to output bindings by calling 'Push-OutputBinding'.
+function generateMatrix
+{
+    Param(
+        [int]$size
+    )
+
+    $matrix = [array]::CreateInstance([array], $size)
+
+    for ($i = 0; $i -lt $size; $i++)
+    {
+        for ($j = 0; $j -lt $size; $j++)
+        {
+            if ($j -eq 0) { $matrix[$i] = [array]::CreateInstance([int], $size) }
+            $matrix[$i][$j] = Get-Random -Minimum 0 -Maximum 99
+        }
+    }
+
+    return $matrix
+}
+
+function formatMatrix
+{
+    Param(
+        [array]$matrix,
+        [int]$size
+    )
+
+    $result = ""
+    $result += "<table>`n"
+
+    for ($i = 0; $i -lt $size; $i++)
+    {
+        $result += "<tr>`n"
+
+        for ($j = 0; $j -lt $size; $j++)
+        {
+            $result += "<td>$($matrix[$i][$j].ToString())</td>`n"
+        }
+
+        $result += "</tr>`n"
+    }
+
+    $result += "</table>`n"
+
+    # $result = $matrix[0][0].ToString() + " " + $matrix[0][1].ToString()
+
+    return $result
+}
+
+function getStyle
+{
+    return "<style>
+        table, th, td {
+            font-family: Arial, Verdana, Helvetica, sans-serif;
+            font-size: 1.3em;
+            text-align: center;
+            border: 1px solid darkgray;
+            border-collapse: collapse;
+            padding: 8px;
+        }
+
+        p {
+            font-family: Arial, Verdana, Helvetica, sans-serif;
+            font-size: 1.3em;
+            text-align: left;
+            padding: 10px;
+        }
+        </style>"
+}
+
+function calculateDeterminant
+{
+    Param(
+        [array]$matrix
+    )
+
+    $size = $matrix.Length
+
+    if ($size -le 2)
+    {
+        return
+    }
+}
+
+$matrix = generateMatrix -size $size
+
+$Body = getStyle
+
+$Body += formatMatrix -matrix $matrix -size $size
+
+$Body += '<p>det = <span style="font-size: 1.5em">AAA</span></p>'
+
+calculateDeterminant -matrix $matrix
+
+# $html = ConvertTo-Html -Body $Body
+
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
     StatusCode = [HttpStatusCode]::OK
-    Body = $body
+    Body = $Body
+    # Body = $html
+    ContentType = "text/html"
 })
