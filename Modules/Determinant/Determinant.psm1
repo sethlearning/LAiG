d6161223670df333b722f230f219c08ea65b1e5e
sethvs <sethvs@users.noreply.github.com>
2021-03-09 15:04:20 +0300

    Refactor code 2

--- Modules/Determinant/Determinant.psm1
+++ Modules/Determinant/Determinant.psm1
@@ -1,1 +1,26 @@
-Export-ModuleMember -Function calculateDeterminant, generateMatrix, subMatrix, formatMatrix, getOne, getStyle
+using namespace System.Net
+
+function Determinant
+{
+    param($Request, $TriggerMetadata)
+    
+    if (-not ($size = $Request.Query.Size)) { $size = 5 }
+    
+    $matrix = generateMatrix -size $size
+    
+    $Body = getStyle
+    
+    $Body += formatMatrix -matrix $matrix -size $size
+    
+    $det = calculateDeterminant -matrix $matrix
+    
+    $Body += "<p>det = <span style=`"font-size: 1.5em`">$det</span></p>"
+    
+    Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+        StatusCode = [HttpStatusCode]::OK
+        Body = $Body
+        ContentType = "text/html"
+    })
+}
+
+Export-ModuleMember -Function Determinant, calculateDeterminant, generateMatrix, subMatrix, formatMatrix, getOne, getStyle
